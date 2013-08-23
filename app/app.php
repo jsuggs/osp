0e5c0a3714216f7c5dc643882cfeaf28f1508113
Jonathon Suggs <jsuggs@murmp.com>
2013-08-23 11:32:28 -0500

    db interaction + category routes

--- app/app.php
+++ app/app.php
@@ -10,8 +10,34 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../src/views',
 ));
 
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/app.db',
+    ),
+));
+
+$app->before(function () use ($app) {
+    $notifications = $app['db']->fetchAssoc('SELECT * FROM notifications');
+    $app['twig']->addGlobal('notifications', $notifications);
+
+    $categories = $app['db']->fetchAll('SELECT * FROM categories');
+    $app['twig']->addGlobal('categories', $categories);
+});
+
 $app->get('/', function() use ($app) {
     return $app['twig']->render('index.twig');
 });
+
+$app->get('/category/{slug}', function ($slug) use ($app) {
+    $sql = "SELECT * FROM categories WHERE slug = ?";
+    $category = $app['db']->fetchAssoc($sql, array($slug));
+
+    return $app['twig']->render('category.twig', array(
+        'category' => $category,
+    ));
+})->bind('category');
 
 return $app;
